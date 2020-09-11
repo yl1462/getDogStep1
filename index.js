a25ededfb38e693fb821c483ea7056f98b4cabe8
yl1462 <yliang1462@hotmail.com>
2020-09-10 19:52:20 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function watchForm() {
+  $('form').submit(function(event) {
+    event.preventDefault();
+    console.log("submit");
+    let number = $('.dogNum').val();
+    console.log(number);
+    getDogImg(number);
+  })
+}
+
+function getDogImg(num) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+  .then(res => res.json())
+  .then(imgResult => { 
+    console.log(imgResult)
+  })
+  .catch(error => console.log(error))
+}
